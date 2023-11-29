e84556a703686063d5f1d1016f60772b57f73e26
dokkoh <ceedje74@gmail.com>
2023-11-29 10:00:33 +0100

    modif footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer role='contentinfo' className='footer'>
+            <span className='footer__elt__small'>Accessibilité : non conforme, en cours de réalisation</span>
             <span className='footer__elt'>© 2023 Dokkoh Développement</span>
             <span className='footer__elt'><Link to="/plan">Plan du site</Link></span>
             <span className='footer__elt'><Link to="/legal-notice">Mentions Légales</Link></span>    
