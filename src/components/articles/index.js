50f6d4606eecf262c2e92746a4633a4b6d6b1c6b
dokkoh <ceedje74@gmail.com>
2023-11-29 14:53:50 +0100

    modif articles.js

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -1,10 +1,12 @@
 import './style.scss';
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import bdm from '../images/bdm.png'
 
 const RecentArticles = () => {
     const [articles, setArticles] = useState([]);
-    const [page, setPage] = useState(1);
+    const [requestCount, setRequestCount] = useState(0);
+    const [error, setError] = useState(null);
 
     const fetchArticles = async () => {
         const apiKey = process.env.REACT_APP_API_KEY_NEWS;
@@ -12,49 +14,63 @@ const RecentArticles = () => {
 
         try {
             const response = await fetch(apiUrl);
-            const data = await response.json();
 
-            if (data.articles) {
+            if (response.ok) {
+                const data = await response.json();
                 setArticles(data.articles);
-                setPage((prevPage) => prevPage + 1);
+                setRequestCount((prevCount) => prevCount + 1);
+            } else {
+                setError('Visitez le');
             }
         } catch (error) {
             console.error('Erreur lors de la récupération des articles:', error);
+            setError('Erreur lors de la récupération des articles');
         }
     };
 
     useEffect(() => {
-        fetchArticles();
-    }, []);
+        if (requestCount < 100) {
+            fetchArticles();
+        }
+    }, [requestCount]);
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-
         return format(date, "dd/MM/yyyy HH:mm");
     };
 
     return (
         <section role='region'>
             <div className='recent-articles'>
-                <h2 className='recent-articles__title'>Articles récents sur le développement web et la tech</h2>
-                <ul className='recent-articles__list'>
-                    {articles.map((article, index) => (
-                        <li className='recent-articles__item' key={index}>
-                            <h3>
-                                <a className='recent-articles__link' href={article.url} target="_blank" rel="noopener noreferrer">
-                                    {article.title}
-                                </a>
-                            </h3>
-                            <img className='recent-articles__img' src={article.image} alt="image en relation avec l'article" />
-                            <p className='recent-articles__desc'>{article.description}</p>
-                            <p className='recent-articles__date'>{formatDate(article.publishedAt)}</p>
-                            <p className='recent-articles__author'>{article.source.name}</p>
-                        </li>
-                    ))}
-                </ul>
+                {error ? (
+                    <React.Fragment>
+                        <h2 className='recent-articles__title'>Articles récents sur le développement web et la tech</h2>
+                        <p>{error} <a className='blogLink' href="https://www.blogdumoderateur.com/" target="_blank" rel="noopener noreferrer">Blog du Modérateur</a>.</p>
+                        <a href="https://www.blogdumoderateur.com/" target="_blank" rel="noopener noreferrer"><img className='recent-articles__img' src={bdm} alt="" /></a>
+                        
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <h2 className='recent-articles__title'>Articles récents sur le développement web et la tech</h2>
+                        <ul className='recent-articles__list'>
+                            {articles.map((article, index) => (
+                                <li className='recent-articles__item' key={index}>
+                                    <h3>
+                                        <a className='recent-articles__link' href={article.url} target="_blank" rel="noopener noreferrer">
+                                            {article.title}
+                                        </a>
+                                    </h3>
+                                    <img className='recent-articles__img' src={article.image} alt="image en relation avec l'article" />
+                                    <p className='recent-articles__desc'>{article.description}</p>
+                                    <p className='recent-articles__date'>{formatDate(article.publishedAt)}</p>
+                                    <p className='recent-articles__author'>{article.source.name}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </React.Fragment>
+                )}
             </div>
         </section>
-
     );
 };
 
